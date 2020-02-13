28b5c8ffa6750ef2c5d39609c89ad7d13d3cb9c5
samo538 <dragonn538@gmail.com>
2020-02-13 12:03:07 +0100

    finish

--- client.py
+++ client.py
@@ -1,12 +1,28 @@
 #192.168.5.233
 import socket
+from io import StringIO
+
+#ip = input("ip: ")
 pack = ('192.168.5.145',12345)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
 
 
 
 
+message = input("zadaj správu": )
+max_data_len = int(input("zadaj dlžku správy: ")) 
 
-sock.sendto("hello".encode("utf-8"),pack)
-
-
+while True:
+    string_file = StringIO(message)
+    if  len(message) > max_data_len:
+        chunk = string_file.read(max_data_len)
+        sock.sendto(chunk.encode("utf-8"),pack)
+    else:
+        sock.sendto(message.encode("utf-8"),pack)
+        break
+    if chunk <= max_data_len:
+       break
+        
+    
+    
--- server.py
+++ server.py
@@ -5,11 +5,12 @@ addr = ('0.0.0.0', 12345)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(addr)
 
-sock.recv(1518)
-
-
-
-
+while True:
+    client_add = sock.recvfrom(1518)
+    message = client_add[0]
+    client = client_add[1]
+    print(message)
+    print(client)
 
 
 
